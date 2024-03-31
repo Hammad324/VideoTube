338bf0b3e4cdf012f239cda0551044b52ea16033
Hammad Farrukh Rohilla <hammadfarrukh602@gmail.com>
2024-03-31 16:45:50 +0500

    added a TODO in user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -255,7 +255,11 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
     return res
     .status(200)
-    .json(200, req.user, "Current user fetched successfully")
+    .json(new ApiResponse(
+        200, 
+        req.user, 
+        "Current user fetched successfully"
+    ))
 })
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -265,7 +269,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
         throw new ApiError(400, "Email and Fullname is required.")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id, 
         {
             $set: {
@@ -364,6 +368,8 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
         new ApiResponse(200, user, "Coverimage is successfully updated.")
     )
 })
+
+// TODO: Delete the previous avatar and coverImage ater uploading the new one 
 
 export {
     registerUser,
